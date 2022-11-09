15c455a36b576bd046d1d95c28075fa00ac5e4db
chanvitj <chanvitj@yahoo.com>
2022-11-09 18:44:05 +0700

    Update app.py
    change uuid

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def Home():
    ipAddr=socket.gethostbyname(hostname)
    return "<h1>Hello JewWithPenTest, "+hostname+", "+ipAddr+"</h1>"
 
-@app.route('/f839c3a1-ec0f-4cc2-b97e-f8d981e9b4d5.html')
+@app.route('/c2f183eb-dc8b-495f-b55a-c674a839760e.html')
 def uuidchecking():
    return ""
 
 @app.route('/forti-uuid.html')
 def uuidDetailCheck():
-   return "<forti-uuid hidden>f839c3a1-ec0f-4cc2-b97e-f8d981e9b4d5</forti-uuid>"
+   return "<forti-uuid hidden>c2f183eb-dc8b-495f-b55a-c674a839760e</forti-uuid>"
 
 if __name__ == '__main__':
  app.debug = True
