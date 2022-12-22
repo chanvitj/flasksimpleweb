9ed86d45c07bd84eaf8b63766c1e3d903c0e7bc3
chanvitj <chanvitj@yahoo.com>
2022-12-22 15:19:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def Home():
    return "<h1>Hello Jew12345677777, "+hostname+", "+ipAddr+"</h1>"
 
 @app.route('/mobileapp')
-def mobileApp():
+def mobileapp():
    return "<h1>This is return from mobile app</h1>"
 
 @app.route('/c2f183eb-dc8b-495f-b55a-c674a839760e.html')
